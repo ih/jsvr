fec5e951a6641b47e861f6b39a07007c63dd2efa
Glitch (jsvr) <none>
2019-11-24 18:19:28 +0000

    create program button

--- public/interpreter-controller.js
+++ public/interpreter-controller.js
@@ -2,6 +2,8 @@
 
 // import Interpreter from './interpreter.js';
 import Interpreter from './interpreter2.js';
+import AugmentedNode from './augmented-node.js';
+import Grammar from './grammar.js';
 
 export default class InterpreterController {
   constructor() {
@@ -14,10 +16,11 @@ export default class InterpreterController {
     this.interpreter.interpret(programElement.dataRepresentation);
     this.interpreter.render();
     this.toggle = this.toggle.bind(this);
+    this.rootElement = document.querySelector('#leftHand');
+
   }
   
   render() {
-    const leftHand = document.querySelector('#leftHand');
     const menu = document.createElement('a-gui-flex-container');
     menu.setAttribute('visible', this.showMenu);
     menu.setAttribute('height', .5);
@@ -30,31 +33,42 @@ export default class InterpreterController {
     menu.setAttribute('item-padding', 0);
     menu.setAttribute('justify-content', 'center');
     menu.setAttribute('align-items', 'normal');
-    
-    const slider = document.createElement('a-gui-slider');
-    slider.setAttribute('height', .25);
-    slider.setAttribute('width', 2);
-    slider.setAttribute('value', 'step');
-    slider.setAttribute('slider-collider', true);
-
-    const button = document.createElement('a-gui-button');
-    button.setAttribute('height', .15);
-    button.setAttribute('width', .5);
-    button.setAttribute('value', 'step');
-    button.setAttribute('font-size', '30px')
-    button.classList.add('triggerable');
+   
+    // create step button
+    const stepButton = document.createElement('a-gui-button');
+    stepButton.setAttribute('height', .15);
+    stepButton.setAttribute('width', .5);
+    stepButton.setAttribute('value', 'step');
+    stepButton.setAttribute('font-size', '30px')
+    stepButton.classList.add('triggerable');
     this.toggle = this.toggle.bind(this);   
-    button.addEventListener('triggerdown', () => {
+    stepButton.addEventListener('triggerdown', () => {
       if (!this.showMenu) {
         return;
       }
       this.interpreter.step();
     });
-    
-    // menu.appendChild(slider);
-    menu.appendChild(button);
-    leftHand.appendChild(menu);
-    leftHand.addEventListener('xbuttondown', this.toggle);
+
+    // new program button
+    const newProgramButton = document.createElement('a-gui-button');
+    newProgramButton.setAttribute('height', .15);
+    newProgramButton.setAttribute('width', .5);
+    newProgramButton.setAttribute('value', 'new program');
+    newProgramButton.setAttribute('font-size', '30px')
+    newProgramButton.classList.add('triggerable');
+    this.toggle = this.toggle.bind(this);   
+    newProgramButton.addEventListener('triggerdown', () => {
+      if (!this.showMenu) {
+        return;
+      }
+      console.log('making a new program');
+      this.createProgram()
+    });
+   
+    menu.appendChild(stepButton);
+    menu.appendChild(newProgramButton);
+    this.rootElement.appendChild(menu);
+    this.rootElement.addEventListener('xbuttondown', this.toggle);
    
     this.menuElement = menu;
   }
@@ -63,6 +77,17 @@ export default class InterpreterController {
     this.showMenu = !this.showMenu;
     
     this.menuElement.setAttribute('visible', this.showMenu);
+  }
+
+  // TODO put this somewhere else? ProgramNode class that inherits from AugmentedNode?
+  async createProgram() {
+    const programNodeData = {type: 'Program', body: [], 'sourType': 'script'};
+    const programNode = new AugmentedNode(programNodeData);
+
+    const programElement = await programNode.render();
+    const position = this.rootElement.getAttribute('position');
+    programElement.setAttribute('position', {x: position.x, y: position.y, z: position.z - .5 });
+    scene.appendChild(programElement);
   }
 }
 
